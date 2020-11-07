5a99d823fc99adbaf34e454b6764c038d79c8191
mzminhaz5683 <mz.minhaz5683@gmail.com>
2020-11-07 17:12:09 +0600

    ann + ml = 88.31

--- diabetes_detection/programs/project_model_v11.py
+++ diabetes_detection/programs/project_model_v11.py
@@ -73,16 +73,16 @@ model_dicty = {
                 'lr_elasticnet' :   model_database.lr_elasticnet,
                 'svc'           :   model_database.svc,
 #                'gbc'           :   model_database.gbc,
-                'lightgbmc'     :   model_database.lightgbmc,
+#                'lightgbmc'     :   model_database.lightgbmc,
                 'xgboostc'      :   model_database.xgboostc,
                 'LogReg'        :   model_database.LogisticRegression,
 #                'knn'           :   model_database.KNeighborsClassifier,
 #                'SVC2'          :   model_database.SVC2,
 #                'decissionTree' :   model_database.DecisionTreeClassifier,
                 'adaboost'      :   model_database.AdaBoostClassifier,
-                'GradientBoost' :   model_database.GradientBoostingClassifier,
+#                'GradientBoost' :   model_database.GradientBoostingClassifier,
 #                'GaussianNB'    :   model_database.GaussianNB,
-                'RabdomForest'  :   model_database.RandomForestClassifier,
+#                'RabdomForest'  :   model_database.RandomForestClassifier,
 #                'ExtraTree'     :   model_database.ExtraTreesClassifier
                 }
 
